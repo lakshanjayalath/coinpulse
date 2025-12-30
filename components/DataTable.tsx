8a139a7631699b3fd5a300e55a28b4b2653e66ef
lakshanjayalath <iamdhananjaya119@gmail.com>
2025-12-30 16:27:55 +0530

    Add Coins page, pagination, and categories components; update package dependencies

--- components/DataTable.tsx
+++ components/DataTable.tsx
@@ -1,14 +1,12 @@
 import {
     Table,
     TableBody,
-    TableCaption,
     TableCell,
     TableHead,
     TableHeader,
     TableRow,
-} from "@/components/ui/table"
-import { cn } from "@/lib/utils"
-
+} from '@/components/ui/table';
+import { cn } from '@/lib/utils';
 
 const DataTable = <T,>({
     columns,
@@ -26,12 +24,14 @@ const DataTable = <T,>({
             <TableHeader className={headerClassName}>
                 <TableRow className={cn('hover:bg-transparent!', headerRowClassName)}>
                     {columns.map((column, i) => (
-                        <TableHead key={i}
+                        <TableHead
+                            key={i}
                             className={cn(
                                 'bg-dark-400 text-purple-100 py-4 first:pl-5 last:pr-5',
                                 headerCellClassName,
                                 column.headClassName,
-                            )}>
+                            )}
+                        >
                             {column.header}
                         </TableHead>
                     ))}
@@ -39,13 +39,18 @@ const DataTable = <T,>({
             </TableHeader>
             <TableBody>
                 {data.map((row, rowIndex) => (
-                    <TableRow key={rowKey(row, rowIndex)} className={cn(
-                        'overflow-hidden rounded-lg border-b border-purple-100/5 hover:bg-dark-400/30! relative',
-                        bodyRowClassName,
-                    )}>
+                    <TableRow
+                        key={rowKey(row, rowIndex)}
+                        className={cn(
+                            'overflow-hidden rounded-lg border-b border-purple-100/5 hover:bg-dark-400/30! relative',
+                            bodyRowClassName,
+                        )}
+                    >
                         {columns.map((column, columnIndex) => (
-                            <TableCell key={columnIndex}
-                                className={cn('py-4 first:pl-5 last:pr-5', bodyCellClassName, column.cellClassName)}>
+                            <TableCell
+                                key={columnIndex}
+                                className={cn('py-4 first:pl-5 last:pr-5', bodyCellClassName, column.cellClassName)}
+                            >
                                 {column.cell(row, rowIndex)}
                             </TableCell>
                         ))}
@@ -53,7 +58,7 @@ const DataTable = <T,>({
                 ))}
             </TableBody>
         </Table>
-    )
-}
+    );
+};
 
-export default DataTable
+export default DataTable;
